876e17128e580ddc3791236f9b7203f6a102097c
Ben Ripkens <bripkens.dev@gmail.com>
2014-01-19 09:24:49 +0100

    React based version running real fast, nice!

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,22 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-    clean: ['app.css'],
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dev: {
+        src: [
+          'vendor/react/react.js',
+          'vendor/promise-0.1.1.min/index.js',
+          'vendor/fuzzy.js/fuzzy.js',
+          'js/index.js'
+        ],
+        dest: 'app.js',
+      },
+    },
+
+    clean: ['app.css', 'app.js'],
 
     sass: {
       dev: {
@@ -20,17 +35,39 @@ module.exports = function(grunt) {
       }
     },
 
+    uglify: {
+      dist: {
+        options: {
+          sourceMap: 'source-map.js'
+        },
+        files: {
+          'app.js': [
+            'vendor/react/react.min.js',
+            'vendor/promise-0.1.1.min/index.js',
+            'vendor/fuzzy.js/fuzzy.min.js',
+            'js/index.js'
+          ]
+        }
+      }
+    },
+
     watch: {
       css: {
         files: 'scss/**/*',
         tasks: ['sass:dev']
+      },
+      js: {
+        files: 'js/**/*',
+        tasks: ['concat:dev']
       }
     }
   });
 
   [
     'grunt-contrib-clean',
+    'grunt-contrib-concat',
     'grunt-contrib-sass',
+    'grunt-contrib-uglify',
     'grunt-contrib-watch'
   ].forEach(grunt.loadNpmTasks.bind(grunt));
 
@@ -38,6 +75,7 @@ module.exports = function(grunt) {
   grunt.registerTask('dev', [
     'clean',
     'sass:dev',
+    'concat:dev',
     'watch'
   ]);
   grunt.registerTask('default', ['dev']);
---
+++ js/index.js
@@ -0,0 +1,245 @@
+(function(context) {
+  'use strict';
+
+  var dom = React.DOM;
+
+  function loadCsv() {
+    return new Promise(function(resolve, reject) {
+      var xhr = new XMLHttpRequest();
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState !== 4) return;
+
+        if (199 < xhr.status && xhr.status < 300) {
+          resolve(xhr.responseText);
+        } else {
+           reject(new Error('Response code is ' + xhr.status));
+        }
+      };
+      xhr.ontimeout = function() {
+        reject(new Error('Request timed out.'));
+      };
+      xhr.timeout = 60000;
+      xhr.open('GET', 'stats.csv', true);
+      xhr.send();
+    });
+  }
+
+
+  function transformToJs(s) {
+    return s.split('\n')
+    .slice(1)
+    .filter(function(line) {
+      return line !== '';
+    })
+    .map(function(line) {
+      var parts = line.split(',');
+      return {
+        name: parts[0],
+        runtimeUsers: parseInt(parts[1], 10),
+        developmentUsers: parseInt(parts[2], 10),
+        pageRank: parseFloat(parts[3])
+      };
+    });
+  }
+
+
+  function sortProjectData(appState) {
+    var ascending = appState.sort.ascending;
+    var prop = appState.sort.property;
+    var getter = null;
+    if (prop === 'users') {
+      getter = function(project) {
+        return project.runtimeUsers + project.developmentUsers;
+      };
+    } else {
+      getter = function(project) {
+        return project[prop];
+      };
+    }
+
+    appState.projects.sort(function(p1, p2) {
+      var v1 = getter(p1);
+      var v2 = getter(p2);
+
+      if (v1 < v2) {
+        return ascending ? -1 : 1;
+      } else if (v1 > v2) {
+        return ascending ? 1 : -1;
+      } else {
+        return 0;
+      }
+    });
+
+    return appState;
+  }
+
+
+  var DataTable = React.createClass({
+    render: function() {
+      var refresher = this.props.refresher;
+      var appState = this.props.appState;
+      var pageLength = appState.pageLength;
+      var page = appState.page;
+      var projects = appState.projects;
+
+      var visibleProjects = [];
+      if (projects.length > 0) {
+        visibleProjects = projects.slice(page * pageLength,
+          Math.min(projects.length, page * pageLength + pageLength));;
+      }
+
+      var setSortProperty = function(prop, defaultOrder, e) {
+        if (appState.sort.property === prop) {
+          appState.sort.ascending = !appState.sort.ascending;
+        } else {
+          appState.sort.ascending = defaultOrder;
+          appState.sort.property = prop;
+        }
+        appState.page = 0;
+        sortProjectData(appState);
+        refresher();
+        e.preventDefault();
+      };
+
+      var th = function(label, prop, defaultOrder) {
+        var className = '';
+        if (appState.sort.property === prop) {
+          if (appState.sort.ascending) {
+            className = 'ascending';
+          } else {
+            className = 'descending';
+          }
+        }
+        return dom.th({
+          href: '#',
+          className: className,
+          onClick: setSortProperty.bind(null, prop, defaultOrder)
+        }, label);
+      };
+
+      return dom.table({},
+        dom.thead({},
+          dom.tr({},
+            th('Name', 'name', true),
+            th('# Users', 'users', false),
+            th('# Runtime Users', 'runtimeUsers', false),
+            th('# Development Users', 'developmentUsers', false),
+            th('Page Rank', 'pageRank', false))),
+        dom.tbody({},
+          visibleProjects.map(function(project) {
+            return dom.tr({},
+              dom.td({}, project.name),
+              dom.td({}, project.runtimeUsers + project.developmentUsers),
+              dom.td({}, project.runtimeUsers),
+              dom.td({}, project.developmentUsers),
+              dom.td({}, project.pageRank));
+          })));
+    }
+  });
+
+
+  var Pagination = React.createClass({
+    render: function() {
+      var refresher = this.props.refresher;
+      var appState = this.props.appState;
+      var projects = appState.projects;
+      var pageLength = appState.pageLength;
+      var page = appState.page;
+      var pageCount = Math.ceil(projects.length / pageLength);
+
+      return dom.ul({className: 'pagination'},
+        dom.li({},
+          dom.a({
+            href: '#',
+            dangerouslySetInnerHTML: {__html: "&laquo;&laquo;"},
+            onClick: function(e) {
+              appState.page = 0;
+              refresher();
+              e.preventDefault();
+            }})),
+        dom.li({},
+          dom.a({
+            href: '#',
+            dangerouslySetInnerHTML: {__html: "&laquo;"},
+            onClick: function(e) {
+              appState.page = Math.max(0, appState.page - 1);
+              refresher();
+              e.preventDefault();
+            }})),
+        dom.li({},
+          dom.span({}, 'Page ' + (page + 1) + ' / ' + pageCount)),
+        dom.li({},
+          dom.a({
+            href: '#',
+            dangerouslySetInnerHTML: {__html: "&raquo;"},
+            onClick: function(e) {
+              appState.page = Math.min(pageCount, appState.page + 1);
+              refresher();
+              e.preventDefault();
+            }})),
+        dom.li({},
+          dom.a({
+            href: '#',
+            dangerouslySetInnerHTML: {__html: "&raquo;&raquo;"},
+            onClick: function(e) {
+              appState.page = pageCount - 1;
+              refresher();
+              e.preventDefault();
+            }})));
+    }
+  });
+
+
+  var LoadingNotification = React.createClass({
+    render: function() {
+      return dom.div({className: 'loading'}, 'Please wait, loading!');
+    }
+  });
+
+
+  var Relato = React.createClass({
+    getInitialState: function() {
+      return {
+        projects: [],
+        page: 0,
+        pageLength: 10,
+        sort: {
+          property: 'pageRank',
+          ascending: false
+        }
+      };
+    },
+
+    componentDidMount: function() {
+      var self = this;
+      loadCsv()
+      .then(transformToJs)
+      .then(function(projects) {
+        var appState = self.getInitialState();
+        appState.projects = projects;
+        sortProjectData(appState);
+        self.setState(appState);
+        return appState;
+      })
+      .catch(function(err) {
+        console.err(err);
+      });
+    },
+
+    render: function() {
+      if (this.state.projects.length === 0) {
+        return LoadingNotification({});
+      }
+      var self = this;
+      var refresher = function() {
+        self.forceUpdate();
+      };
+
+      return dom.div({},
+        DataTable({appState: this.state, refresher: refresher}),
+        Pagination({appState: this.state, refresher: refresher}));
+    }
+  });
+
+  React.renderComponent(Relato({}), document.getElementById('npm-data'));
+})(this);
